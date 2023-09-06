665134c099f95a466a291ee2e272a75991e27d32
Narendra Kumar <narendra@kashware.com>
2023-09-06 12:42:29 +0530

    user model changes new profile

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -8,6 +8,23 @@ const UserSchema = mongoose.Schema({
     interest: { type: Array, default: [] },
     isVerified: Boolean,
     location: Map,
+    title: String,
+    bio: String,
+    dob: String,
+    education: [{
+        etype: { type: String, enum: ['school', 'college'] },
+        name: String,
+        specialisation: String,
+        degree: String,
+        start: Date,
+        end: Date
+    }],
+    profession: [{
+        name: String,
+        position: String,
+        start: Date,
+        end: Date
+    }],
     name: { type: String, trim: true },
     userName: { type: String, trim: true, lowercase: true, unique: true },
     profilePath: { type: String, trim: true, default: 'nopicture.circle' },
